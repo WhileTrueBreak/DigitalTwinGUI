f6c65eadc669fda91d1d66cd5a4099f43285ae3c
Jeffrey Chen <jeff.jc2003@gmail.com>
2023-02-20 14:09:47 +1100

    made it easier to add streams for cams scene

--- scenes/CamScene.py
+++ scenes/CamScene.py
@@ -17,33 +17,6 @@ class CamScene(Scene):
 
     def createUi(self):
         btnPadding = 5
-        constraints = [
-            COMPOUND(RELATIVE(T_X, 0, P_W), ABSOLUTE(T_X, btnPadding)),
-            ABSOLUTE(T_Y, btnPadding),
-            COMPOUND(RELATIVE(T_W, 0.25, P_W), ABSOLUTE(T_W, -2 * btnPadding)),
-            ABSOLUTE(T_H, 30)
-        ]
-        btn, text = centeredTextButton(self.window, constraints)
-        btn.setDefaultColor((1,1,1))
-        text.setText('stream 1')
-        text.setFontSize(24)
-        text.setTextSpacing(7)
-        text.setTextColor((0,0,0))
-        self.camBtns.append(btn)
-
-        constraints = [
-            COMPOUND(RELATIVE(T_X, 0, P_W), ABSOLUTE(T_X, btnPadding)),
-            COMPOUND(ABSOLUTE(T_Y, 30 + 2 * btnPadding), ABSOLUTE(T_Y, btnPadding)),
-            COMPOUND(RELATIVE(T_W, 0.25, P_W), ABSOLUTE(T_W, -2 * btnPadding)),
-            ABSOLUTE(T_H, 30)
-        ]
-        btn, text = centeredTextButton(self.window, constraints)
-        btn.setDefaultColor((1,1,1))
-        text.setText('stream 2')
-        text.setFontSize(24)
-        text.setTextSpacing(7)
-        text.setTextColor((0,0,0))
-        self.camBtns.append(btn)
 
         constraints = [
             COMPOUND(RELATIVE(T_X, 0.5, P_W), ABSOLUTE(T_X, btnPadding)),
@@ -53,6 +26,21 @@ class CamScene(Scene):
         ]
         self.streams.append(UiStream(self.window, constraints, 'http://172.31.1.225:8080/?action=streams'))
         self.streams.append(UiStream(self.window, constraints, 'http://172.31.1.226:8080/?action=streams'))
+
+        for i in range(len(self.streams)):
+            constraints = [
+                COMPOUND(RELATIVE(T_X, 0, P_W), ABSOLUTE(T_X, btnPadding)),
+                COMPOUND(ABSOLUTE(T_Y, (30 + 2 * btnPadding)*i), ABSOLUTE(T_Y, btnPadding)),
+                COMPOUND(RELATIVE(T_W, 0.25, P_W), ABSOLUTE(T_W, -2 * btnPadding)),
+                ABSOLUTE(T_H, 30)
+            ]
+            btn, text = centeredTextButton(self.window, constraints)
+            btn.setDefaultColor((1,1,1))
+            text.setText(f'stream {i+1}')
+            text.setFontSize(24)
+            text.setTextSpacing(7)
+            text.setTextColor((0,0,0))
+            self.camBtns.append(btn)
         self.sceneWrapper.addChildren(*self.camBtns)
 
         constraints = [
