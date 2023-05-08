f18ab7e3fb40debaf62b9e5f0e89fae40f754211
whiletrue <jche0470@student.monash.edu>
2023-05-08 17:26:24 +1000

    removed unused shaders

--- res/shader/ui/guiFragment.glsl
+++ res/shader/ui/guiFragment.glsl
@@ -6,11 +6,16 @@ in vec4 fColor;
 in vec2 fTexCoords;
 in float fTexID;
 flat in uint fUiId;
+// flat in vec2 nearestPos;
+// in vec2 InterpNearestPos;
 
 layout (location = 0) out vec4 color;
 layout (location = 1) out uvec3 picking;
 
 void main() {
+	// float dist = distance(InterpNearestPos, nearestPos);
+	// color = vec4(dist, 0, 0, 1);
+	// if(dist < 0.1) discard;
 	if(fTexID > -0.5){
 		color = fColor * texture(uTextures[int(fTexID)], fTexCoords);
 	}else{
--- res/shader/ui/guiVertex.glsl
+++ res/shader/ui/guiVertex.glsl
@@ -9,6 +9,8 @@ out vec4 fColor;
 out vec2 fTexCoords;
 out float fTexID;
 flat out uint fUiId;
+// flat out vec2 nearestPos;
+// out vec2 InterpNearestPos;
 
 void main() {
     gl_Position = vec4(aPos, 0.0, 1.0);
@@ -16,4 +18,6 @@ void main() {
     fTexCoords = aTexCoords;
     fTexID = aTexID;
     fUiId = uint(aUiId);
+    // nearestPos = aPos;
+    // InterpNearestPos = aPos;
 }
