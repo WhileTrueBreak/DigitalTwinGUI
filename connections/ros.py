c5cdf5594ed714402b2579c40c12687b0170ea24
whiletrue <jche0470@student.monash.edu>
2023-05-17 10:37:14 +1000

    added ros file

---
+++ connections/ros.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import rospy
+import sys
+from std_msgs.msg import String
+
+class simple_class:
+
+    def __init__(self):
+        self.sub = rospy.Subscriber("chatter",String,self.callback)
+
+    def callback(self,data):
+        print(data.data)
+
+
+
+def main(args):
+    obc = simple_class()
+    rospy.init_node('simple_class', anonymous=True)
+    while not rospy.core.is_shutdown():
+        rospy.rostime.wallsleep(0.5)
+    print("Shutting down")
+
+if __name__ == '__main__':
+    main(sys.argv)
