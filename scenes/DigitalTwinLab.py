237a3e29fdb5bbf26e55849a706666b648d418bf
whiletrue <jche0470@student.monash.edu>
2023-07-04 14:19:41 +1000

    added furniture

--- scenes/DigitalTwinLab.py
+++ scenes/DigitalTwinLab.py
@@ -41,6 +41,7 @@ class DigitalTwinLab(Scene):
 
         self.__createRoom()
         self.__addRobots()
+        self.__addFurniture()
         return
     
     def __createRoom(self):
@@ -81,7 +82,7 @@ class DigitalTwinLab(Scene):
             [(14.1,7),(15.3,7),(0.885,2.4)],
             [(12.32,7),(13.26,7),(0.885,2.4)],
 
-            [(0.3,7),(1.3,7),(0.885,2.4)],
+            [(0,7),(1.3,7),(0.885,2.4)],
             [(2.15,7),(4,7),(0.885,2.4)],
             [(4.84,7),(5.94,7),(0.885,2.4)],
 
@@ -133,28 +134,28 @@ class DigitalTwinLab(Scene):
         self.genericModels = []
         self.arms = {}
 
-        base = GenericModel(self.window, self.modelRenderer, Assets.KUKA_FLEX, createTransformationMatrix(10, 3, 0.89, 0, 0, 0))
+        base = GenericModel(self.window, self.modelRenderer, Assets.KUKA_FLEX, createTransformationMatrix(5, 4.9, 0.89, 0, 0, 180))
         arm = KukaRobotTwin(self.window, createTransformationMatrix(0.315, 0, 0, 0, 0, 0), 21, 'R1', self.modelRenderer, hasForceVector=True, hasGripper=True)
         arm.setLiveColors([(0.5, i/8, 1.0, 0.7)for i in range(9)])
         arm.setTwinColors([(1.0, 0.5, i/8, 0.0)for i in range(9)])
         self.genericModels.append(base)
         self.arms[base] = arm
 
-        base = GenericModel(self.window, self.modelRenderer, Assets.KUKA_FLEX, createTransformationMatrix(10, 5, 0.89, 0, 0, 0))
+        base = GenericModel(self.window, self.modelRenderer, Assets.KUKA_FLEX, createTransformationMatrix(8.5, 0.75, 0.89, 0, 0, 90))
         arm = KukaRobotTwin(self.window, createTransformationMatrix(0.315, 0, 0, 0, 0, 0), 22, 'R2', self.modelRenderer, hasForceVector=True, hasGripper=True)
         arm.setLiveColors([(0.5, i/8, 1.0, 0.7)for i in range(9)])
         arm.setTwinColors([(1.0, 0.5, i/8, 0.0)for i in range(9)])
         self.genericModels.append(base)
         self.arms[base] = arm
         
-        base = GenericModel(self.window, self.modelRenderer, Assets.OMNIMOVE, createTransformationMatrix(11, 4, 0.7, 0, 0, 0))
+        base = GenericModel(self.window, self.modelRenderer, Assets.OMNIMOVE, createTransformationMatrix(13, 3, 0.7, 0, 0, 180))
         arm = KukaRobotTwin(self.window, createTransformationMatrix(0.363, -0.184, 0, 0, 0, -90), 23, 'R3', self.modelRenderer, hasForceVector=True, hasGripper=True)
         arm.setLiveColors([(0.5, i/8, 1.0, 0.7)for i in range(9)])
         arm.setTwinColors([(1.0, 0.5, i/8, 0.0)for i in range(9)])
         self.genericModels.append(base)
         self.arms[base] = arm
         
-        base = GenericModel(self.window, self.modelRenderer, Assets.OMNIMOVE, createTransformationMatrix(11, 2, 0.7, 0, 0, 0))
+        base = GenericModel(self.window, self.modelRenderer, Assets.OMNIMOVE, createTransformationMatrix(11, 3, 0.7, 0, 0, 0))
         arm = KukaRobotTwin(self.window, createTransformationMatrix(0.363, -0.184, 0, 0, 0, -90), 24, 'R4', self.modelRenderer, hasForceVector=True, hasGripper=True)
         arm.setLiveColors([(0.5, i/8, 1.0, 0.7)for i in range(9)])
         arm.setTwinColors([(1.0, 0.5, i/8, 0.0)for i in range(9)])
@@ -162,6 +163,18 @@ class DigitalTwinLab(Scene):
         self.arms[base] = arm
 
         # [self.modelRenderer.setColor(model.modelId, (1,1,1,0.5)) for model in self.genericModels]
+
+    def __addFurniture(self): 
+        self.shelves = []
+        self.shelves.append(self.modelRenderer.addModel(Assets.SHELF, createTransformationMatrix(14.70,7,0,0,0,-90)))
+        self.shelves.append(self.modelRenderer.addModel(Assets.SHELF, createTransformationMatrix(12.66,7,0,0,0,-90)))
+        self.shelves.append(self.modelRenderer.addModel(Assets.SHELF, createTransformationMatrix(11.35,7,0,0,0,-90)))
+        self.shelves.append(self.modelRenderer.addModel(Assets.SHELF, createTransformationMatrix(10.75,7,0,0,0,-90)))
+
+        self.tables = []
+        self.tables.append(self.modelRenderer.addModel(Assets.TABLES[1], createTransformationMatrix(2.5,7-1.05,0.85,0,0,0)))
+        self.tables.append(self.modelRenderer.addModel(Assets.TABLES[2], createTransformationMatrix(4.8,7-0.9,0.85,0,0,0)))
+        self.tables.append(self.modelRenderer.addModel(Assets.TABLES[1], createTransformationMatrix(6.8,7-1.05,0.85,0,0,0)))
 
     def handleUiEvents(self, event):
         [arm.handleEvents(event) for arm in self.arms.values()]
