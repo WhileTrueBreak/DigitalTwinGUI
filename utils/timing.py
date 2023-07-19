af787a5850a37f86535cbf14ef960a3bbc1a9b6a
whiletrue <jche0470@student.monash.edu>
2023-07-19 11:41:06 +1000

    change variable name

--- utils/timing.py
+++ utils/timing.py
@@ -2,7 +2,7 @@ import time
 
 from colorama import Fore, Back, Style
 
-HIGHEST = 0
+MAX_F = 0
 MAX_N = 0
 LAYER = 0
 
@@ -10,7 +10,7 @@ total = [0]
 
 def timing(func):
     def wrapper(*arg, **kw):
-        global HIGHEST, LAYER, MAX_N
+        global MAX_F, MAX_N, LAYER
         LAYER += 1
         total.append(0)
         start = time.time_ns()
@@ -22,9 +22,9 @@ def timing(func):
         fl = len(func.__qualname__)+LAYER*2
         tl = len(f'{round(dt)}')
         MAX_N = max(MAX_N, tl)
-        HIGHEST = max(fl, HIGHEST)
+        MAX_F = max(fl, MAX_F)
         tab = f'{Fore.CYAN}|{Style.RESET_ALL} '
-        print(f"{tab*LAYER}{func.__qualname__} time:{' '*(HIGHEST-fl)} {Fore.RED}{round(dt)}µs{Style.RESET_ALL}{' '*(MAX_N-tl)} {Fore.BLUE}-{round(tracked)}µs{Style.RESET_ALL}")
+        print(f"{tab*LAYER}{func.__qualname__} time:{' '*(MAX_F-fl)} {Fore.RED}{round(dt)}µs{Style.RESET_ALL}{' '*(MAX_N-tl)} {Fore.BLUE}-{round(tracked)}µs{Style.RESET_ALL}")
         LAYER -= 1
         return out
     return wrapper
