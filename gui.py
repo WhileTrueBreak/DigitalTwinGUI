7d5d2d8849f453fa845d4f2b714bf550f5c0536e
whiletrue <jche0470@student.monash.edu>
2023-07-26 17:03:36 +1000

    removed test scene

--- gui.py
+++ gui.py
@@ -4,24 +4,22 @@ import nest_asyncio
 from window import *
 
 from scenes.digitalTwinLab import *
-from scenes.testScene import *
 
 from asset import *
 
 from colorama import init as colorama_init
 
 colorama_init(convert=True)
-
 nest_asyncio.apply()
 
-window = Window((1200, 800), 'Digital Twin GUI', fullscreen=False, resizeable=True, vsync=False)
+if __name__ == "__main__":
 
-labScene = DigitalTwinLab(window, 'Digital Twin Lab')
-labScene.createUi()
-testScene = TestScene(window, 'test')
-testScene.createUi()
+    window = Window((1200, 800), 'Digital Twin GUI', fullscreen=False, resizeable=True, vsync=False)
 
-window.addScene(labScene)
+    labScene = DigitalTwinLab(window, 'Digital Twin Lab')
+    labScene.createUi()
 
-window.createUi()
-window.run()
+    window.addScene(labScene)
+
+    window.createUi()
+    window.run()
