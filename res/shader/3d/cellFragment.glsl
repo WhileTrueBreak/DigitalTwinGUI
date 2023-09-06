a07b9ff2272424a00bce86c66c66c09b9be93ff6
whiletrue <jche0470@student.monash.edu>
2023-09-06 14:35:56 +1000

    .

--- res/shader/3d/cellFragment.glsl
+++ res/shader/3d/cellFragment.glsl
@@ -15,20 +15,25 @@ void main() {
 
 	uvec2 v = texelFetch(picking, coords, 0).xy;
 
-	if(v.y == 0){
-		frag = vec4(0,0,1,1);
+	if(v.x%7==0){
+		frag = vec4(float(v.x)/80,0,0,1);
 	}
-	if(v.y == 1){
-		frag = vec4(0,1,0,1);
+	if(v.x%7==1){
+		frag = vec4(0,float(v.x)/80,0,1);
 	}
-	if(v.y == 2){
-		frag = vec4(0,1,1,1);
+	if(v.x%7==2){
+		frag = vec4(float(v.x)/80,float(v.x)/80,0,1);
 	}
-	if(v.y == 3){
-		frag = vec4(1,0,0,1);
+	if(v.x%7==3){
+		frag = vec4(0,0,float(v.x)/80,1);
 	}
-	if(v.y == 4){
-		frag = vec4(1,0,1,1);
+	if(v.x%7==4){
+		frag = vec4(float(v.x)/80,0,float(v.x)/80,1);
 	}
-
+	if(v.x%7==5){
+		frag = vec4(0,float(v.x)/80,float(v.x)/80,1);
+	}
+	if(v.x%7==6){
+		frag = vec4(float(v.x)/80,float(v.x)/80,float(v.x)/80,1);
+	}
 }
