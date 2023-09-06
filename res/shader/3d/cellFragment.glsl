ca7132e679c18cf7ebb8adca458ca397e6a0c0d6
whiletrue <jche0470@student.monash.edu>
2023-09-06 14:29:29 +1000

    testing picking

--- res/shader/3d/cellFragment.glsl
+++ res/shader/3d/cellFragment.glsl
@@ -7,30 +7,28 @@ uniform vec2 texture_dim;
 in vec2 texPos;
 
 uniform sampler2D screen;
-uniform sampler2D objects;
+uniform usampler2D picking;
 
 void main() {
 
-	float step_u = 0.5 / texture_dim[0];
-	float step_v = 0.5 / texture_dim[1];
+	ivec2 coords = ivec2(gl_FragCoord.xy);
 
-    ivec2 iTexPos = ivec2(texPos[0]*texture_dim[0], texPos[1]*texture_dim[1]);
+	uvec2 v = texelFetch(picking, coords, 0).xy;
 
-	vec4 centerPixel = texelFetch(objects, iTexPos, 0);
-
-	vec4 rightPixel  = texelFetch(objects, iTexPos + ivec2(1, 0), 0);
-	vec4 bottomPixel = texelFetch(objects, iTexPos + ivec2(0, 1), 0);
-	vec4 leftPixel  = texelFetch(objects, iTexPos + ivec2(-1, 0), 0);
-	vec4 topPixel = texelFetch(objects, iTexPos + ivec2(0, -1), 0);
-
-	float _dFdXp = length(rightPixel.xy - centerPixel.xy);
-	float _dFdYp = length(bottomPixel.xy - centerPixel.xy);
-	float _dFdXn = length(leftPixel.xy - centerPixel.xy);
-	float _dFdYn = length(topPixel.xy - centerPixel.xy);
-	float d = _dFdXp + _dFdYp + _dFdXn + _dFdYn;
-    
-    vec4 sp = texture2D(screen, texPos);
-
-    frag = vec4(centerPixel.x/10, centerPixel.y/10, 1, 1);
+	if(v.y == 0){
+		frag = vec4(0,0,1,1);
+	}
+	if(v.y == 1){
+		frag = vec4(0,1,0,1);
+	}
+	if(v.y == 2){
+		frag = vec4(0,1,1,1);
+	}
+	if(v.y == 3){
+		frag = vec4(1,0,0,1);
+	}
+	if(v.y == 4){
+		frag = vec4(1,0,1,1);
+	}
 
 }
